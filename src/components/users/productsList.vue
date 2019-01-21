db256c95c563c1e01b00686a6e0b4264b57e63a1
Vijaya Boppana <vijayaboppana@vijaya.local>
2019-01-21 17:51:46 +0530

    productlist

---
+++ src/components/users/productsList.vue
@@ -0,0 +1,147 @@
+<template>
+
+    <div class="container">
+    <router-view></router-view>
+      
+    <div
+    id="e3"
+    style="max-width: 800px; margin: auto;" 
+    class="grey lighten-3"
+    >
+    
+    <v-layout></v-layout>
+    <v-card>
+    <v-container class="mt-7">
+      <v-layout>
+        <v-flex xs12>
+            <v-card color="cyan darken-2" class="white--text">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src= "static/Hauei.png"
+                    height="200px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <h1 class="display-3">Iphone 6S</h1>
+                      <div>PRICE RANGE:1000-20,000</div>
+                      <div></div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              <!-- <v-divider light></v-divider> -->
+              <v-card-actions class="pa-3">
+                <v-btn >shop now</v-btn>
+              </v-card-actions>
+              </v-layout>
+            </v-card>
+          </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container class="mt-7">
+      <v-layout>
+        <v-flex xs12>
+            <v-card color="cyan darken-2" class="white--text" height="200px">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+                    height="200px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <h1 class="display-3">Iphone 6S</h1>
+                      <div>PRICE RANGE:1000-20,000</div>
+                     
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              <!-- <v-divider light></v-divider> -->
+              <v-card-actions class="pa-3">
+                <v-btn >shop now</v-btn>
+              </v-card-actions>
+              </v-layout>
+            </v-card>
+          </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container class="mt-7">
+      <v-layout>
+        <v-flex xs12>
+            <v-card color="cyan darken-2" class="white--text" height="200px">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+                    height="200px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <h1 class="display-3">Iphone 6S</h1>
+                      <div>IPRICE RANGE:1000-20,000</div>
+                      
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              <!-- <v-divider light></v-divider> -->
+              <v-card-actions class="pa-3">
+                <v-btn >shop now</v-btn>
+              </v-card-actions>
+              </v-layout>
+            </v-card>
+          </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container class="mt-7">
+      <v-layout>
+        <v-flex xs12>
+            <v-card color="cyan darken-2" class="white--text" height="200px">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+                    height="200px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <h1 class="display-3">Samsung</h1>
+                      <div>Price Range:1000-20,000</div>
+                     
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              <!-- <v-divider light></v-divider> -->
+              <v-card-actions class="pa-3">
+                <v-btn >shop now</v-btn>
+              </v-card-actions>
+              </v-layout>
+            </v-card>
+          </v-flex>
+      </v-layout>
+    </v-container>
+    </v-card>
+    </div>
+    </div>
+    
+
+</template>
+<script>
+export default {
+    name: 'productsList',
+   
+    
+  }
+
+</script>
